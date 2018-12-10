b0c1cf7bd00bd50adb26d44b66eba273264ef723
Everett Penner <everetpenner@gmail.com>
2018-12-10 10:14:04 -0600

    minified images with gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
-let cleanCSS = require('gulp-clean-css');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('prefix', () =>
     gulp.src('src/css/*.css')
@@ -16,3 +17,19 @@ gulp.task('minify-css', () => {
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css/'));
 });
+
+gulp.task('img-min', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('dist/images'))
+);
